96af0c5a59cfd1297cd9168b8db873b83254eb60
Aeron Potkek <56285457+potkek01@users.noreply.github.com>
2021-04-01 00:41:07 -0300

    sim

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1617247670,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/block.png",
+		"images/player-sheet0.png",
+		"images/pato-sheet0.png",
+		"images/pato-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground3.png",
+		"media/pega o pato.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
